c875daf22cad91946c2b7256df7b52109289ce30
KyungjinLee <bramhome5848@gmail.com>
2024-04-29 18:16:15 +0900

    20240429 섹션10. 스프링 트랜잭션 전파1 - 기본 - 3.전파 기본

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -88,4 +88,45 @@ class BasicTxTest {
         log.info("트랜잭션2 롤백");
         txManager.rollback(tx2);
     }
+
+    /**
+     * 스프링 트랜잭션 전파(트랜잭션 전파 옵션 REQUIRED)
+     - 외부 트랜잭션 수행중, 내부 트랜잭션 추가로 수행
+     - outer -> true, inner -> false
+     - 내부 트랜잭션을 시작하는 시점에 이미 외부 트랜잭션은 진행중이며 이 경우 내부 트랜잭션이 외부 트랜잭션에 참여
+     -> 내부 트랜잭션이 외부 트랜잭션을 그대로 이어 받아 따름
+     -> 외부 트래잭션(논리 트랜잭션1)과 내부 트랜잭션(논리 트랜잭션2)이 하나의 물리 트랜잭션으로 묶이는 것
+
+     * 커밋
+     - 아래 코드의 경우 커밋을 두 번 호출
+     - 트랜잭션을 생각해보면 하나의 커넥션에 커밋을 한 번만 호출 할 수 있음 -> 커밋이나 롤백을 하면 해당 트랜잭션은 끝남
+     - 로그 확인 결과 내부 트랜잭션 커밋의 경우 DB 커넥션을 통해 커밋하는 로그를 확인할 수 없음
+     -> 외부 트랜잭션만 물리 트랜잭션을 시작하고, 커밋
+     -> 내부 트랜잭션이 실제 물리 트랜잭션을 커밋하면 트랜잭션이 끝나버리기 때문에, 처음 시작한 외부 트랜잭션까지 이어갈 수 없음
+     -> 내부 트랜잭션은 DB 커넥션을 통한 물리 트랜잭션은 커밋하면 안됨
+     -> 스프링은 여러 트랜잭션이 함께 사용될 경우 처음 트랜잭션을 시작한 외부 트랜잭션이 실제 물리 트랜잭션을 관리하도록 함
+
+     * 정리
+     - 트랜잭션 매니저에 커밋을 호출한다고 해서 항상 실제 커넥션에 물리 커밋이 발생하지는 않음
+     - 신규 트랜잭션인 경우에만 실제 커넥션을 사용해서 물리 커밋과 롤백을 수행, 신규 트랜잭션이 아닌 경우 실제 물리 커넥션을 사용하지 않음
+     - 트랜잭션이 내부에서 추가로 사용되면 트랜잭션 매니저에 커밋하는 것이 항상 물리 커밋으로 이어지지 않음
+     -> 논리 트랜잭션과 물리 트랜잭션으로 나누어 설명
+     - 트랜잭션이 내부에서 추가로 사용되면 트랜잭션 매니저를 통해 논리 트랜잭션을 관리하고, 모든 논리 트랜잭션이 커밋되면 물리 트랜잭션이 커밋!
+     */
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction() = {}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
 }
